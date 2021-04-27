8f3864ff2c48aa976d60b0b757bd398a8c3f6f08
Connor Dieck <connordieck@gmail.com>
2021-04-27 15:28:16 -0400

    finished answers (txt file) and exercise code (js file)

---
+++ app.js
@@ -0,0 +1,32 @@
+// var obj = {
+//     numbers: {
+//       a: 1,
+//       b: 2
+//     }
+//   };
+  
+//   var a = obj.numbers.a;
+//   var b = obj.numbers.b;
+
+/* Write an ES2015 Version */
+
+const obj = {
+    numbers : {
+        a: 1,
+        b: 2
+    }
+};
+
+const {a, b} = obj.numbers;
+
+// var arr = [1, 2];
+// var temp = arr[0];
+// arr[0] = arr[1];
+// arr[1] = temp;
+
+/* Write an ES2015 Version */
+
+let arr = [1, 2];
+[arr[0], arr[1]] = [arr[1], arr[0]];
+
+const raceResults = ([first, second, third, ...rest]) => ({first, second, third, rest})
